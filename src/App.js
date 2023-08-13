c347dbb2a8ef5ec5d1a33d806a3497fb0de7d159
Valentin Plokhotniouk <vplokhot@Valentins-MacBook-Pro.local>
2023-08-13 13:46:06 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
-import ReactDOM from "react-dom/client";
-import Audio from "./AudioRecorder";
-import { Recorder } from "./Recorder";
 import { Splant } from "./Splant";
-// require("dotenv").config();
 
 function App() {
   return (
